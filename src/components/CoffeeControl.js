01bedf1f47908906b4217223833adc58243ec930
Marcus <marcusferreira23@gmail.com>
2022-08-11 12:08:41 -0700

    add local state.

---
+++ src/components/CoffeeControl.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NewCoffeeForm from './NewCoffeeForm';
+import CoffeeList from './CoffeeList';
+
+class CoffeeControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default CoffeeControl;
---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewCoffeeForm(props){
+  return (
+    <React.Fragment>
+      <h3>Fake Coffee Form</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewCoffeeForm;
