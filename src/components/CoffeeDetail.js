e39f44a356498f3f993af8355ad9d24c106859d1
Marcus <marcusferreira23@gmail.com>
2022-08-12 12:50:18 -0700

    add temporary CSS to Coffee Detail component.

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -2,10 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function CoffeeDetail(props){
+  const coffeeDetailStyles = {
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  }
   const { coffee, onClickingSellPound } = props;
 
   return (
     <React.Fragment>
+      <div style={coffeeDetailStyles}>
       <h1>Coffee Detail</h1>
       <h3>{coffee.name} - {coffee.origin}</h3>
       <p><em>Roast: <b>{coffee.roast}</b></em></p>
@@ -13,6 +19,7 @@ function CoffeeDetail(props){
       <p><em>Pounds Left: <b>{coffee.total}</b></em></p>
       <button onClick={()=> onClickingSellPound(coffee.id) }>Sell Pound</button>
       <hr/>
+      </div>
     </React.Fragment>
   );
 }
